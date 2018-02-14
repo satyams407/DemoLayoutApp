adfda2e2a2a6db8a0c7ebfbc53cb1cb301c41b37
Satyam Sehgal <satyams407@outlook.com>
2018-02-14 11:00:57 +0530

    update viewcontroller

--- DemoLayoutApp/ViewController.swift
+++ DemoLayoutApp/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 import LocalAuthentication
 
 class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+   
+    @IBAction func touchIdButton(_ sender: Any) {
+        self.enableTouchIdAlert("Would you like to enable Touch Id?",
+                                err: "TouchId enables biometric access to mBriefing for all fingerprints enabled on this device")
+    }
     
     //strings declarations
     let okay : String = "Ok"
@@ -19,14 +29,7 @@ class ViewController: UIViewController {
     let unauthorizedUser : String = "Not a Authorised User"
     let touchIdLogin : String = "Please use touch Id to Login"
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
     
-    
-    @IBAction func touchIdButton(_ sender: Any) {
-        self.authenticationUsingTouchID()
-    }
     
     func authenticationUsingTouchID(){
         let authContext = LAContext()
@@ -34,43 +37,73 @@ class ViewController: UIViewController {
         
         if(authContext.canEvaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, error: &authError)){
             
-            //touch Id is available on the device
-            authContext.evaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, localizedReason: self.touchIdLogin, reply: {(success, error) -> Void in
+           //touch Id is available
+            authContext.evaluatePolicy(LAPolicy.deviceOwnerAuthentication, localizedReason: self.touchIdLogin, reply: {(success, error) -> Void in
                 if success {
-                
+
                     // push to new ViewController (WelcomePage)
                     let storyBoard = UIStoryboard(name: "Main", bundle:nil)
                     let objSomeViewController = storyBoard.instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
-                    
-                    let randomQueue = DispatchQueue(label: "q1", qos : .userInteractive)
-                   
-                     randomQueue.async {
-                        print("in dealock 1")
-                        randomQueue.sync {
-                             print("in dealock 2")
+
+                    DispatchQueue.main.async {
                         self.navigationController?.pushViewController(objSomeViewController, animated: true)
-                      }
                     }
-//                    DispatchQueue.main.async {
-//                       self.navigationController?.pushViewController(objSomeViewController, animated: true)
-//                   }
                 }
                 else{
                     self.notifyUser(self.authFailed, err: self.unauthorizedUser)
                 }
-             })
+            })
         }else{
             
-            //touchID not available
-            self.notifyUser(self.touchIdNotAvailable, err: self.errorMsg)
+         //touchID not available or not configured
+       if #available(iOS 11.0, *) {
+                switch authError!.code{
+
+                case LAError.Code.biometryNotEnrolled.rawValue:
+                   UIApplication.shared.open(NSURL(string: UIApplicationOpenSettingsURLString)! as URL)
+
+                case LAError.Code.passcodeNotSet.rawValue:
+                    self.notifyUser("A passcode has not been set",
+                                    err: authError?.localizedDescription)
+                    UIApplication.shared.open(NSURL(string: UIApplicationOpenSettingsURLString)! as URL)
+
+                default:
+                    self.notifyUser(self.touchIdNotAvailable, err: self.errorMsg)
+                }
+            } else {
+                print("ios 10 or less than")
+                switch authError!.code{
+
+                case LAError.Code.touchIDNotEnrolled.rawValue:
+                    UIApplication.shared.open(NSURL(string: UIApplicationOpenSettingsURLString)! as URL)
+
+                case LAError.Code.passcodeNotSet.rawValue:
+                    self.notifyUser("A passcode has not been set",
+                                    err: authError?.localizedDescription)
+                    UIApplication.shared.open(NSURL(string: UIApplicationOpenSettingsURLString)! as URL)
+
+                default:
+                    self.notifyUser(self.touchIdNotAvailable, err: self.errorMsg)
+                }
+            }
         }
     }
     
-    
     func notifyUser(_ msg: String, err: String?) {
         let alert = UIAlertController(title: msg, message: err, preferredStyle: .alert)
-        
         let cancelAction = UIAlertAction(title: okay, style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func enableTouchIdAlert(_ msg: String, err: String?){
+        let alert = UIAlertController(title: msg, message: err, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "YES" , style: .default, handler: { (action) -> Void in
+            self.authenticationUsingTouchID()
+          
+        })
+        let cancelAction = UIAlertAction(title: "SKIP", style: .cancel, handler: nil)
+        alert.addAction(okAction)
         alert.addAction(cancelAction)
         self.present(alert, animated: true, completion: nil)
     }
